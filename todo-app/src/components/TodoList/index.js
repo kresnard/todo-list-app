6d5feb97c0a5137dd3fa6d25c31af6852c3a0f57
Kresna Rangga Darmansyah <kresnard@gmail.com>
2022-08-16 14:11:56 +0700

    edit todolist

--- todo-app/src/components/TodoList/index.js
+++ todo-app/src/components/TodoList/index.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import TodoCard from '../TodoCard';
 
-const Index = () => {
+import './style.css';
+
+const Index = props => {
+    console.log('props from list', props)
+
+    const _deleteTodoHandler = id => {
+        props.deleteTodos(id);
+    };
+
     return (
-        <div>index</div>
-    )
-}
+        <div>
+            {props.todos.map(todo => {
+                return (
+                    <TodoCard 
+                    key={todo.id}
+                    name={todo.name}
+                    desc={todo.desc}
+                    deleteHandler={() => _deleteTodoHandler(todo.id)}
+                    />
+                );
+            })}
+        </div>
+    );
+};
 
 export default Index;
