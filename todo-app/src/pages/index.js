b54527b565c5ea7ab83a85d959fee1215fbd5a0b
Kresna Rangga Darmansyah <kresnard@gmail.com>
2022-08-16 14:25:34 +0700

    edit pages

--- todo-app/src/pages/index.js
+++ todo-app/src/pages/index.js
@@ -1,16 +1,41 @@
-import React from 'react'
-import Todoadd from '../components/TodoAdd'
-import Todolist from '../components/TodoList'
-import './style.css'
+import React, { useState, useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+import Todoadd from '../components/TodoAdd';
+import Todolist from '../components/TodoList';
 
 const Index = () => {
+    const LOCAL_STORAGE_KEY = 'list-todos';
+    const [todos, setTodos] = useState([]);
+
+    const addTodoHandler = todo => {
+        const newTodo = [{ id:uuidv4(), name: todo.name, desc: todo.desc}];
+        setTodos([...todos, newTodo]);
+        localStorage.setItem(
+            LOCAL_STORAGE_KEY,
+            JSON.stringify([...todos,...newTodo])
+        );
+    };
+
+    const deleteTodoHandler = id => {
+        const updatedTodos = todos.filter(todo => todo.id !== id);
+        setTodos(updatedTodos);
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(updatedTodos));
+    };
+
+    useEffect(() => {
+        const listTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+        listTodos && setTodos(listTodos);
+    }, [setTodos]);
+
+
     return (
-        <div className='pages'>
+        <div>
             <h1>Hello, Create Your Activity!</h1>
-            <Todoadd />
-            <Todolist />
+            <Todoadd addHandler={addTodoHandler} />
+            <Todolist todos={todos} deleteTodos={deleteTodoHandler} />
         </div>
-    )
-}
+    );
+};
 
 export default Index;
